42a5d27b00d01b5b822327c0aa8267dee38fed22
Duc Filan <ducfilan@gmail.com>
2016-03-30 01:16:38 +0700

    edit matching youtube link

--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ for (var i = 0; i < youtubeIframe.length; i++) {
   var thisObj = youtubeIframe[i];
 
   var src = thisObj.getAttribute("src");
-  if (src != undefined && src.indexOf("https://www.youtube.com") > -1) {
+  if (src != undefined && src.indexOf("www.youtube.com") > -1) {
     thisObj.setAttribute("src", src.replace("autoplay=1", "autoplay=0"));
   }
 }
